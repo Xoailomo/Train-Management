2484ee60c7516149a6b7e0289a42123b2a2f6975
phank <phankhoatn2011@gmail.com>
2024-09-24 19:11:28 +0700

    first commit

---
+++ trainBooking/src/trainbooking/TrainBooking.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trainbooking;
+
+/**
+ *
+ * @author phank
+ */
+public class TrainBooking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
