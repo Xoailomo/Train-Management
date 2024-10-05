23acc0c05a072c4297347b64157793f95de22746
Mango <144985828+Xoailomo@users.noreply.github.com>
2024-10-05 19:19:38 +0700

    Update viewMenu.java

--- Train/src/view/viewMenu.java
+++ Train/src/view/viewMenu.java
@@ -30,20 +30,53 @@ public class viewMenu {
         }
         System.out.println("0.exit");
     }
-    // truyền dữ liệu cho train List
-    public void loadTrainData(){
-        String content[]={"tcode", "name","dstation", "astation",
-                          "dtime"," seat","booked", "atime"};
-        System.out.println("**** load train data **** ");
-        for (int i = 0; i < content.length; i++) {
-            System.out.println((i + 1) + ". " + content[i]);
-        }
-        System.out.println("0. extit");
+       public void displayTrain(TrainList tl){
+        String header[]={"tcode", "train_name", "seat", "booked",
+            "depart_time","depart_place", "available_seat"};
+        int[] style ={10,20,10,20,15,15,15};
+        // in ra header
+        tableBorder(style);
+        tableHearder(style, header);
+        tableBorder(style);
         
+        TrainNode n = tl.head;
+        while(n!=null){
+            Train t = n.info;
+            tableHearder(style, new String[]{
+                t.tcode+"",t.name+"",t.seat+"",t.booked+"",t.dtime+"",
+                t.dstation+"",t.seat-t.booked+""
+            });
+            tableBorder(style);
+            n=n.next;
+        }
     }
-    public void tableHearder(){
-    
-    
+    public void tableHearder(int[] style, String[] header) {
+        for(int i=0;i<style.length;i++){
+            System.out.print("|");
+                for(int j=0;j<style[i];j++){
+                    System.out.println(tableCell(style[i],header[i]));
+                }
+                System.out.println("|");
+        }
+    }
+    public String tableCell(int style, String content){
+        String s = content; // ? de lam gi
+        int  offset = style - content.length();
+        for(int i=0;i<style;i++){
+            content +=" ";
+        }
+        return content;
+    }
+
+    public void tableBorder(int[] style) {
+        for (int i = 0; i < style.length; i++) {
+            System.out.print("+");
+            for (int j = 0; j < style[i]; j++) {
+                System.out.print("-");
+            }
+        }
+        System.out.println("+");
+    }
     
     
     
